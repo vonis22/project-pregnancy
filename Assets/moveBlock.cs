648ab2f8a8211aa81655ec4579d5110ed5c0ae1f
spammailenquetes@gmail.com <spammailenquetes@gmail.com@9f4bfe06-53c2-5a30-2f5f-f1a8989f89b1>
2015-01-06 12:04:35 +0000

    - Rotation moet nog 1 op 1 worden - bewegen en draaien versnellen werkt

--- Assets/moveBlock.cs
+++ Assets/moveBlock.cs
@@ -5,12 +5,23 @@ public class moveBlock : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		//transform.rotation.y = Input.acceleration.y *10;
 	}
 	
 	// Update is called once per frame
-	void Update () 
+		void Update () 
 	{
-		transform.Rotate(Input.acceleration.x, 0, -Input.acceleration.z);
+		Vector3 acceleration = new Vector3 (Input.acceleration.x, Input.acceleration.z, Input.acceleration.y);
+		//float xAxis = Input.acceleration.x;
+		//float zAxis = Input.acceleration.z;
+		//float yAxis = Input.acceleration.y;
+		Vector3 rotate = transform.rotation;
+
+		rotate = Input.acceleration;
+
+		//transform.Rotate(xAxis, yAxis, zAxis);
+	
+		Debug.Log (acceleration);
+
 	}
 }
