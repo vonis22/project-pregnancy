cee162eee0962ca57f4be30445e99860fb946d65
Klunsy1995@gmail.com <Klunsy1995@gmail.com@9f4bfe06-53c2-5a30-2f5f-f1a8989f89b1>
2015-01-16 16:01:45 +0000

    LichtFunctie

--- branches/lichtintensiteit/licht.cs
+++ branches/lichtintensiteit/licht.cs
@@ -1,23 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class licht : MonoBehaviour {
-
-
-	public float Kleur= 0.0f;
-
-void Start()
+public class licht : MonoBehaviour
 {
-	
-}
+	public float Kleur = 0;
 
-void Update()
-{
-		if(Kleur==1.0f){
+	void Start()
+	{
+		
+	}
 
-		renderer.material.color = new Color (30, 0, 0,0);
+	void Update()
+	{
+		if (Kleur == 1)
+		{
+			renderer.material.color = new Color (30, 0, 0,0);
 		}
 
-	
-}
+		if (Kleur == 2)
+		{
+			renderer.material.color = new Color (0, 30, 0,0)
+		}
+
+		if (Kleur == 3)
+		{
+			renderer.material.color = new Color (0, 0, 30,0)
+		}
+	}
 }
