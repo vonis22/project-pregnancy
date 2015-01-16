4e0193774e42d2c43cc6c2864b90802d7137ffe7
Klunsy1995@gmail.com <Klunsy1995@gmail.com@9f4bfe06-53c2-5a30-2f5f-f1a8989f89b1>
2015-01-16 16:01:45 +0000

    LichtFunctie

--- lichtintensiteit/Assets/scripts/licht.cs
+++ lichtintensiteit/Assets/scripts/licht.cs
@@ -1,23 +1,168 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class licht : MonoBehaviour {
-
-
-	public float Kleur= 0.0f;
-
-void Start()
+public class licht : MonoBehaviour
 {
-	
-}
+	public float Kleur = 0.0f;
 
-void Update()
-{
-		if(Kleur==1.0f){
+	void Start()
+	{
 
-		renderer.material.color = new Color (30, 0, 0,0);
+	}
+
+	void Update()
+	{
+		//Rood
+		if(Kleur == 1)
+		{
+			renderer.material.color = new Color (255.0f/255, 0.0f/255, 0.0f/255, 1);
 		}
 
-	
-}
+		if(Kleur == 11)
+		{
+			renderer.material.color = new Color (255.0f/255, 0.0f/255, 0.0f/255, 0.9f);
+		}
+
+		if(Kleur == 12)
+		{
+			renderer.material.color = new Color (255.0f/255, 0.0f/255, 0.0f/255, 0.8f);
+		}
+
+		if(Kleur == 13)
+		{
+			renderer.material.color = new Color (255.0f/255, 0.0f/255, 0.0f/255, 0.7f);
+		}
+
+		if(Kleur == 14)
+		{
+			renderer.material.color = new Color (255.0f/255, 0.0f/255, 0.0f/255, 0.6f);
+		}
+
+		if(Kleur == 15)
+		{
+			renderer.material.color = new Color (255.0f/255, 0.0f/255, 0.0f/255, 0.5f);
+		}
+
+		if(Kleur == 16)
+		{
+			renderer.material.color = new Color (255.0f/255, 0.0f/255, 0.0f/255, 0.4f);
+		}
+
+		if(Kleur == 17)
+		{
+			renderer.material.color = new Color (255.0f/255, 0.0f/255, 0.0f/255, 0.3f);
+		}
+
+		if(Kleur == 18)
+		{
+			renderer.material.color = new Color (255.0f/255, 0.0f/255, 0.0f/255, 0.2f);
+		}
+
+		if(Kleur == 19)
+		{
+			renderer.material.color = new Color (255.0f/255, 0.0f/255, 0.0f/255, 0.1f);
+		}
+
+		//Oranje
+		if(Kleur == 2)
+		{
+			renderer.material.color = new Color (255.0f/255, 127.0f/255, 36.0f/255, 1);
+		}
+
+		if(Kleur == 21)
+		{
+			renderer.material.color = new Color (255.0f/255, 127.0f/255, 36.0f/255, 1);
+		}
+
+		if(Kleur == 22)
+		{
+			renderer.material.color = new Color (255.0f/255, 127.0f/255, 36.0f/255, 1);
+		}
+
+		if(Kleur == 23)
+		{
+			renderer.material.color = new Color (255.0f/255, 127.0f/255, 36.0f/255, 1);
+		}
+
+		if(Kleur == 24)
+		{
+			renderer.material.color = new Color (255.0f/255, 127.0f/255, 36.0f/255, 1);
+		}
+
+		if(Kleur == 25)
+		{
+			renderer.material.color = new Color (255.0f/255, 127.0f/255, 36.0f/255, 1);
+		}
+
+		if(Kleur == 26)
+		{
+			renderer.material.color = new Color (255.0f/255, 127.0f/255, 36.0f/255, 1);
+		}
+
+		if(Kleur == 27)
+		{
+			renderer.material.color = new Color (255.0f/255, 127.0f/255, 36.0f/255, 1);
+		}
+
+		if(Kleur == 28)
+		{
+			renderer.material.color = new Color (255.0f/255, 127.0f/255, 36.0f/255, 1);
+		}
+
+		if(Kleur == 29)
+		{
+			renderer.material.color = new Color (255.0f/255, 127.0f/255, 36.0f/255, 1);
+		}
+
+		//Groen
+		if(Kleur == 3)
+		{
+			renderer.material.color = new Color (0.0f/255, 255.0f/255, 0.0f/255, 1);
+		}
+
+		if(Kleur == 31)
+		{
+			renderer.material.color = new Color (0.0f/255, 255.0f/255, 0.0f/255, 1);
+		}
+
+		if(Kleur == 32)
+		{
+			renderer.material.color = new Color (0.0f/255, 255.0f/255, 0.0f/255, 1);
+		}
+
+		if(Kleur == 33)
+		{
+			renderer.material.color = new Color (0.0f/255, 255.0f/255, 0.0f/255, 1);
+		}
+
+		if(Kleur == 34)
+		{
+			renderer.material.color = new Color (0.0f/255, 255.0f/255, 0.0f/255, 1);
+		}
+
+		if(Kleur == 35)
+		{
+			renderer.material.color = new Color (0.0f/255, 255.0f/255, 0.0f/255, 1);
+		}
+
+		if(Kleur == 36)
+		{
+			renderer.material.color = new Color (0.0f/255, 255.0f/255, 0.0f/255, 1);
+		}
+
+		if(Kleur == 37)
+		{
+			renderer.material.color = new Color (0.0f/255, 255.0f/255, 0.0f/255, 1);
+		}
+
+		if(Kleur == 38)
+		{
+			renderer.material.color = new Color (0.0f/255, 255.0f/255, 0.0f/255, 1);
+		}
+
+		if(Kleur == 39)
+		{
+			renderer.material.color = new Color (0.0f/255, 255.0f/255, 0.0f/255, 1);
+		}
+	}
 }
