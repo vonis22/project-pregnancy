77ae30b71b035c274e18040c24a3552d471f83c0
patrick.volleybal@hotmail.com <patrick.volleybal@hotmail.com@9f4bfe06-53c2-5a30-2f5f-f1a8989f89b1>
2015-01-19 22:09:23 +0000

    alles,

---
+++ branches/Jeff testing zone/Assets/scripts/playPhase.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+
+public class playPhase : MonoBehaviour {
+	
+	public cubesDraaien script;
+
+	public List<float> maxY = new List<float>();
+	public List<float> minY = new List<float>();
+	public List<float> breathInput = new List<float>();
+	public List<float> breathOutput = new List<float>();
+	public List<float> fullBreathList = new List<float>();
+
+	public float breathInTimer = 0.0f;
+	public float breathOutTimer = 0.0f;
+	public float fullBreath = 0.0f;
+
+	void Start ()
+	{
+		GameObject mainHandler = GameObject.FindGameObjectWithTag("MainCamera");
+		script = mainHandler.GetComponent<cubesDraaien>();
+	}
+
+	void Update () 
+	{
+		//print (script.avgY);
+
+			float tmpY = script.currentY;
+			
+			if (script.currentY < script.avgY)
+			{
+				//Uitgeademd
+				minY.Add(tmpY); //
+				breathOutTimer += Time.deltaTime;
+				breathInput.Add(breathInTimer);
+				breathInTimer = 0.0f; //Wordt
+			}
+			else
+			{
+				//Ingeademd
+				maxY.Add(tmpY);
+				breathInTimer += Time.deltaTime;
+				breathOutput.Add(breathOutTimer);
+				breathOutTimer = 0.0f;
+			}
+
+		if(breathOutput.Last() != 0)
+		{
+			fullBreathList.Add (breathOutput.Last());
+			breathOutput.Clear (); //Maakt de array leeg
+		}
+
+		if(breathInput.Last() != 0)
+		{
+			fullBreathList.Add (breathInput.Last());
+			breathInput.Clear ();
+		}
+
+	}
+}
---
+++ branches/Jeff testing zone/Assets/scripts/pressButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pressButton : MonoBehaviour {
+
+	public GameObject handler_prefab;
+	public Vector3 pos;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1") ) {
+			Instantiate(handler_prefab);
+			Destroy(gameObject);
+		}
+	
+	}
+}
