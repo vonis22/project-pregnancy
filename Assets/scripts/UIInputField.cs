38f70b816b4d6489d7816830361ea4c135256ec2
Klunsy1995@gmail.com <Klunsy1995@gmail.com@9f4bfe06-53c2-5a30-2f5f-f1a8989f89b1>
2015-01-25 13:19:39 +0000

    Timers

--- Assets/scripts/UIInputField.cs
+++ Assets/scripts/UIInputField.cs
@@ -2,14 +2,66 @@
 using System.Collections;
 using UnityEngine.UI;
 
-public class UIInputField : MonoBehaviour {
-	public Text InputText;
+public class UIInputField : MonoBehaviour
+{
+	public Text charText;
+	public Text placeholderText;
+	public changeScene script;
+	bool Error = false;
+	public float errorTimer;
+	public float outcome;
+	public string aantal;
+
 	// Use this for initialization
-	void Start () {
-		InputText = GameObject.Find ("InputText").GetComponent<Text>();
+	void Start ()
+	{
+		charText = GameObject.Find ("CharText").GetComponent<Text> ();
+		placeholderText = GameObject.Find ("Placeholder").GetComponent<Text> ();
+		script = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<changeScene> ();
 	}
+
+	public void Update ()
+	{
+		if (errorTimer < 0)
+		{
+			Error = false;
+		}
+
+		errorTimer -= Time.deltaTime;
+	}
+
 	public void CharacterField(string inputFieldString)
 	{
-		InputText.text = inputFieldString;
+		charText.text = inputFieldString;
+	}
+
+	public void SubmitFunction ()
+	{
+		if (charText.text.Length <= 3 && charText.text != "0" && charText.text != "00" && charText.text != "000")
+		{
+			PlayerPrefs.SetString("Aantal", charText.text);
+			aantal = PlayerPrefs.GetString("Aantal");
+			outcome = float.Parse(aantal);
+			script.ChangeToScene("Level_1");
+		}
+
+		else if (placeholderText.enabled == true)
+		{
+			Error = true;
+			errorTimer = 2.0f;
+		}
+	}
+
+ 	void OnGUI()
+	{
+		
+		GUIStyle avgFont = new GUIStyle ();
+		avgFont.fontSize = 14;
+		avgFont.normal.textColor = Color.red;
+
+		if (Error) 
+		{
+			GUI.Label(new Rect(Screen.width / 2 -135 ,Screen.height / 2, 150,150), "Voer een aantal in", avgFont);
+		}
 	}
 }
